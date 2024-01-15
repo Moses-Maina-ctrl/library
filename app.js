51c6b52342be6a06c3470f30e290a12d9327a49b
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-01-16 00:20:40 +0300

    Created showBooks function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const bookName = document.querySelector('#bookName');
 const authorName = document.querySelector('#authorName');
 const statuse = document.querySelector('#status');
+const bookDisplay = document.querySelector("#bookDisplay")
 const myLibrary = [];
   
 function book(title, author, read) {
@@ -12,16 +13,22 @@ function book(title, author, read) {
  function addBookToLibrary() {
   let bookNameValue =  bookName.value;
   let authorNameValue =authorName.value;
+  
   let statusValue = statuse.value;
 
   let newBook = new book( bookNameValue, authorNameValue, statusValue)
   myLibrary.push(newBook);
+  console.log(myLibrary);
   if (myLibrary.length == 0){
-  
+    bookDisplay.innerText = "Please add books"
   }else{
-    for (let i=0; i<myLibrary.length; i++){
+   
+    bookDisplay.innerText = "There is  books"
 
-    }
+    // for (let i=0; i<myLibrary.length; i++){
+//TODO function to show books
+
+    //}
   }
  }
 
